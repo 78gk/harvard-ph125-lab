bb026c55b9d131a60b24e55335ac3ede180344e5
Kirubel Tewodros <kirutew17654321@gmail.com>
2026-05-20 15:16:46 +0300

    ci: add GitHub Actions workflow to run scripts and upload artifacts; add answers summary and Section 7 scaffold

---
+++ scripts/section7_answers.R
@@ -0,0 +1,11 @@
+## Template for Section 7 answers
+## Copy this file to implement solutions for Section 7 exercises.
+
+library(tidyverse)
+library(dslabs)
+
+set.seed(1)
+
+## TODO: add Section 7 code here
+
+cat("Section 7 scaffold created. Start implementing exercises in this file.\n")
